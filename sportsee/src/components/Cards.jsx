3eda174a615c3e50d8be9f8e6557fb271c494127
Nellylie <marie.beka01@gmail.com>
2023-09-27 23:14:16 +0200

    feat(): affichage des cards infos utilisateur

---
+++ sportsee/src/components/Cards.jsx
@@ -0,0 +1,19 @@
+function Cards (props) {
+
+  return (
+    <div>
+      <div>
+        <img src={props.icon} alt="icon" />
+      </div>
+      <div>
+        <div>
+          {props.number}
+          {props.unit}
+        </div>
+        <div>{props.type}</div>
+      </div>
+
+    </div>
+  );
+}
+export default Cards;
--- sportsee/src/pages/Profil.jsx
+++ sportsee/src/pages/Profil.jsx
@@ -1,12 +1,99 @@
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+import { useEffect, useState } from "react";
+import { getDatasSection } from '../services/ModelisationData'; 
+import { useParams } from "react-router-dom";
+import Barschart from '../componentsRecharts/Barschart';
+import iconCalories from '../icones/energy.svg';
+import iconCarbs from '../icones/apple.svg';
+import iconProtein from '../icones/chicken.svg';
+import iconFat from '../icones/cheeseburger.svg';
+import Cards from "../components/Cards";
+
+
+
+
 
 function Profil () {
+  const idUser = useParams().id;
+  const [datas, setDatas] = useState(null);
+  const [isDataLoading, setDataLoading] = useState(true);
+  const [apiStatut] = useState(false);
+  console.log("userid", idUser, "datas 22", datas);
+
+  const cardData = [
+    {
+      icon: iconCalories,
+      number: datas?.userDatas?.keyData?.calorieCount,
+      type: "Calories",
+      unit: "kCal",
+    },
+    {
+      icon: iconProtein,
+      number: datas?.userDatas?.keyData?.proteinCount,
+      type: "Proteines",
+      unit: "g",
+    },
+    {
+      icon: iconCarbs,
+      number: datas?.userDatas?.keyData?.carbohydrateCount,
+      type: "Glucides",
+      unit: "g",
+    },
+    {
+      icon: iconFat,
+      number: datas?.userDatas?.keyData?.lipidCount,
+      type: "Lipides",
+      unit: "g",
+    },
+  ];
+
+  useEffect(() => {
+    const url = "http://localhost:3000";
+    console.log("datamock", url +'/datas/datasMocked.json');
+    const fetchData = async () => {
+      try {
+        if (apiStatut) {
+          setDatas(await getDatasSection(undefined, parseInt(idUser), apiStatut));
+        } else {
+          setDatas(await getDatasSection(url + '/datas/datasMocked.json', parseInt(idUser)));
+        }
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setDataLoading(false);
+      }
+    };
+  
+    fetchData();
+    return () => {
+    };
+  }, [apiStatut, idUser]);
+  
+  console.log('Routes file', datas);
+  
+  if (isDataLoading) return null; 
+  
   return (
     <div>
       <Header/>
       <Sidebar/>
-      PROFIL
+      <main>
+        <Barschart data = {datas?.activitiesDatas?.sessions}/>
+        <div className = "cards">
+          <div>
+            {cardData.map((card, index) => (
+              <Cards
+                key={index}
+                icon={card.icon} 
+                number={card.number} 
+                type={card.type} 
+                unit={card.unit} 
+              />
+            ))}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
