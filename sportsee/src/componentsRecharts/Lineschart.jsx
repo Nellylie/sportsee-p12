f7c1f9e87278158d853f30c82badaf017243653e
Nellylie <marie.beka01@gmail.com>
2023-10-28 17:21:51 +0200

    feat(): mouvement de la souris recuperé pour l'effet gradient du lineschart

--- sportsee/src/componentsRecharts/Lineschart.jsx
+++ sportsee/src/componentsRecharts/Lineschart.jsx
@@ -1,4 +1,5 @@
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import { useState } from 'react';
 
 function SessionsTooltip({ payload, active }) {
   if (active && payload && payload.length) {
@@ -11,9 +12,19 @@ function SessionsTooltip({ payload, active }) {
 
   return null;
 }
+
 function Lineschart({data}) {
+  const [gradientPosition, setGradientPosition] = useState(50);
+
+  const handleMouseMove = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left; 
+    const percentage = (x / rect.width) * 100;
+    setGradientPosition(percentage);
+  };
+
   return (
-    <div style={{ backgroundColor: 'red', borderRadius:'5px', padding:'5px', width: '200px', height: '200px', aspectRatio: '1/1' }}>
+    <div onMouseMove={handleMouseMove} style={{ backgroundColor: 'red', background: `linear-gradient(to right, red ${gradientPosition -5}%, rgb(200,0,0) ${gradientPosition}%)`, borderRadius:'5px', padding:'5px', width: '200px', height: '200px', aspectRatio: '1/1' }}>
       <ResponsiveContainer width="100%" height="100%" aspect={1}>
         <LineChart
           data={data}
